99fa43e6725e6681d6620a95faa302b3237a805c
lin <451846939@qq.com>
2024-04-18 16:49:38 +0800

    test

--- soccr/soccr.c
+++ soccr/soccr.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <arpa/inet.h>
 #include "soccr.h"
 
 #ifndef SIOCOUTQNSD
@@ -609,9 +610,9 @@ static int send_fin(struct libsoccr_sk *sk, struct libsoccr_sk_data *data, unsig
 		     sk->dst_addr->v4.sin_port);
 	}
 
-	loge("IPv4-mapped IPv6 address src_v4 prot:(%hu)  dst_v4 prot:(%hu) \n", sk->src_addr->v6.sin6_port,
+	loge("IPv4-mapped IPv6 address src_v6:(%s) prot:(%hu)  dst_v6:(%s) prot:(%hu) \n",inet_ntoa(htonl(src_v4)), sk->src_addr->v6.sin6_port,inet_ntoa(htonl(dst_v4)),
 	     sk->dst_addr->v6.sin6_port);
-	loge("IPv4-mapped IPv4 address src_v4 prot:(%hu)  dst_v4 prot:(%hu) \n", sk->src_addr->v4.sin_port,
+	loge("IPv4-mapped IPv4 address src_v4:(%s) prot:(%hu)  dst_v4:(%s) prot:(%hu) \n", inet_ntoa(htonl(src_v4)),sk->src_addr->v4.sin_port, inet_ntoa(htonl(dst_v4)),
 	     sk->dst_addr->v4.sin_port);
 	if (family == AF_INET6)
 		libnet_type = LIBNET_RAW6;
